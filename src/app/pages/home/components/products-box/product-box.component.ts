c16733bce264b1c8cfd187c647ed713df0bb9f17
Courtney Carson <cmacarson@gmail.com>
2023-09-05 15:03:43 -0700

    set up product box, cart event handler

--- src/app/pages/home/components/products-box/product-box.component.ts
+++ src/app/pages/home/components/products-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'product-box',
@@ -6,8 +7,23 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false; //if someone clicks on the product box, it will be full width
+  product: Product | undefined = {
+    id: 1,
+    title: 'sneakers',
+    price: 150,
+    category: 'shoes',
+    description: 'description',
+    image: 'https://via.placeholder.com/150',
+  };
+
+  @Output() addToCart: EventEmitter<Product> = new EventEmitter<Product>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAddToCart(): void {
+    console.log('add to cart');
+    this.addToCart.emit(this.product);
+  }
 }
