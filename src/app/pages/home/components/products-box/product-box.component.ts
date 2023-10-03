6348d7b5f57cc9d4aefd354a954b5fb0ab7d35e4
Courtney Carson <cmacarson@gmail.com>
2023-10-02 17:52:27 -0700

    interpolate data and implement the fake store api

--- src/app/pages/home/components/products-box/product-box.component.ts
+++ src/app/pages/home/components/products-box/product-box.component.ts
@@ -7,14 +7,7 @@ import { Product } from 'src/app/models/product.model';
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false; //if someone clicks on the product box, it will be full width
-  product: Product | undefined = {
-    id: 1,
-    title: 'sneakers',
-    price: 150,
-    category: 'shoes',
-    description: 'description',
-    image: 'https://via.placeholder.com/150',
-  };
+  @Input() product: Product | undefined;
 
   @Output() addToCart: EventEmitter<Product> = new EventEmitter<Product>();
 
