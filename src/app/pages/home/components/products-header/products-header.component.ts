14b405d6648d82cb7140d5193b97aef5db282296
Courtney Carson <cmacarson@gmail.com>
2023-10-03 18:04:08 -0700

    sort and asc/desc work

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -9,18 +9,23 @@ export class ProductsHeaderComponent {
   itemsShowCount = 12;
 
   @Output() columnCountChange = new EventEmitter<number>();
+  @Output() itemsCountChange = new EventEmitter<number>();
+  @Output() sortChange = new EventEmitter<string>();
 
   onSortUpdated(newSort: string): void {
     this.sort = newSort;
-    console.log('sort val', this.sort);
+    this.sortChange.emit(newSort);
+    // console.log('sort val', this.sort);
   }
+
   onItemsUpdated(count: number): void {
     this.itemsShowCount = count;
-    console.log('itemsShowCount val', this.itemsShowCount);
+    this.itemsCountChange.emit(count);
+    // console.log('itemsShowCount val', this.itemsShowCount);
   }
 
   onColumnsUpdated(columnsNumber: number): void {
-    console.log('prodsheadercount emittingg', columnsNumber);
+    // console.log('prodsheadercount emittingg', columnsNumber);
     this.columnCountChange.emit(columnsNumber);
   }
 }
