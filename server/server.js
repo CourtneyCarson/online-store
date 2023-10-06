754da6a1029519635589ab533db7064dfe125993
Courtney Carson <cmacarson@gmail.com>
2023-10-05 19:34:44 -0700

    customers can add their shipping address on stripe checkout page before payment

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const bodyparser = require('body-parser');
 
 const app = express();
 app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 app.use(cors({ origin: true, credentials: true }));
 
 const stripe = require('stripe')(
@@ -18,52 +17,52 @@ app.post('/checkout', async (req, res, next) => {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       shipping_address_collection: {
-      allowed_countries: ['US', 'CA'],
+        allowed_countries: ['US', 'CA'],
       },
       shipping_options: [
         {
-            shipping_rate_data: {
+          shipping_rate_data: {
             type: 'fixed_amount',
             fixed_amount: {
-                amount: 0,
-                currency: 'usd',
+              amount: 0,
+              currency: 'usd',
             },
             display_name: 'Free shipping',
             // Delivers between 5-7 business days
             delivery_estimate: {
-                minimum: {
+              minimum: {
                 unit: 'business_day',
                 value: 5,
-                },
-                maximum: {
+              },
+              maximum: {
                 unit: 'business_day',
                 value: 7,
-                },
-            }
-            }
+              },
+            },
+          },
         },
         {
-            shipping_rate_data: {
+          shipping_rate_data: {
             type: 'fixed_amount',
             fixed_amount: {
-                amount: 1500,
-                currency: 'usd',
+              amount: 1500,
+              currency: 'usd',
             },
             display_name: 'Next day air',
             // Delivers in exactly 1 business day
             delivery_estimate: {
-                minimum: {
+              minimum: {
                 unit: 'business_day',
                 value: 1,
-                },
-                maximum: {
+              },
+              maximum: {
                 unit: 'business_day',
                 value: 1,
-                },
-            }
-            }
+              },
+            },
+          },
         },
-        ],
+      ],
       line_items: req.body.items.map((item) => ({
         price_data: {
           currency: 'usd',
@@ -79,10 +78,11 @@ app.post('/checkout', async (req, res, next) => {
       success_url: 'http://localhost:4242/success.html',
       cancel_url: 'http://localhost:4242/cancel.html',
     });
+
     res.status(200).json(session);
   } catch (error) {
     next(error);
   }
 });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(4242, () => console.log('app is running on 4242'));
